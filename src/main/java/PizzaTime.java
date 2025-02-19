56ebd659d08cc98534789cea6f2a0cf82d0f4204
Ghost-void <25grahab@retsdstudent.ca>
2025-02-19 17:04:03 +0000

    name and nuber

--- src/main/java/PizzaTime.java
+++ src/main/java/PizzaTime.java
@@ -3,6 +3,15 @@ public class PizzaTime {
 
     public static void main(String[] args){
         System.out.println("Pizza Time!");
+        String location = "";
+
+        System.out.println("Whats your streets name?: ");
+        String streetname = scanner.nextLine();
+        scanner.nextLine();
+        System.out.println("What is your street number?; ");
+        Int number = scanner.nextInt();
+        scanner.nextLine();
         
     }
+    
 }
