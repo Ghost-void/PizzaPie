f745e335084e32e8f4009114e853f51ce799086f
Ghost-void <25grahab@retsdstudent.ca>
2025-02-24 17:11:28 +0000

    area left

--- src/main/java/PizzaTime.java
+++ src/main/java/PizzaTime.java
@@ -9,7 +9,7 @@ public class PizzaTime {
         boolean bvalid = false;
         boolean tvalid = false;
         boolean gvalid = false;
-        while(!svalid){
+            while(!svalid){
 
             String[] stringArray;
             stringArray = new String[]{"Burrows", "Main", "Henderson", "Magnus", "Mountain"};
@@ -32,27 +32,33 @@ public class PizzaTime {
         }
         System.out.println(svalid);
         }
+        System.out.println("What is your street number?; ");
         while (!scanner.hasNextInt()){
             scanner.nextLine();
-            System.out.println("What is your street number?; ");
+            
             
             scanner.nextLine();
             System.out.println("Wrong, this needs to be a number");
             
         }
             int number = scanner.nextInt();
+            scanner.nextLine();
         while(!tvalid){
             System.out.println("what city are you in?; ");
             String city = scanner.nextLine();
             scanner.nextLine();
-            if(city != "Winnipeg"){
+            if(!city.equals("Winnipeg")){
                 System.out.println("Sorry but we don't deliver outside of Winnipeg");
+            }
+            else if (city.equals("Winnipeg")){
+                tvalid = true;
             }
         }
         while(!gvalid){
             System.out.println("what is your area code?: ");
             //in a A1A-1A1 format
             String area = scanner.nextLine();
+            scanner.nextLine();
             char firstchar = area.charAt(0);
             boolean isFirstCharLetter = Character.isLetter(firstchar); // Returns true
             System.out.println(isFirstCharLetter);
@@ -60,6 +66,7 @@ public class PizzaTime {
             boolean issecCharLetter = Character.isLetter(secchar); // Returns true
             char thirchar = area.charAt(5);
             boolean isthirCharLetter = Character.isLetter(thirchar); // Returns true
+            
         }
         
     }
